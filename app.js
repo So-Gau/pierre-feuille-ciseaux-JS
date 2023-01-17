23bada5c6f69df41862583f85e28598e5afe7535
So <wolfiaspikky@gmail.com>
2023-01-17 12:21:42 +0100

    add variable

---
+++ app.js
@@ -0,0 +1,14 @@
+let resetBtn = document.getElementById('reset');
+let scoreJoueur = document.getElementById('score-joueur');
+let scoreOrdinateur = document.getElementById('score-ordinateur');
+// permet de recupérer les trois bouttons individuellement=[...]
+let btnJoueur = [...document.getElementsByClassName('btn-joueur')];
+let opierreBtn = document.getElementById('opierre');
+let ofeuilleBtn = document.getElementById('ofeuille');
+let ociseaux = document.getElementById('ociseaux');
+let message  = document.getElementById('message');
+let nextBtn  = document.getElementById('next');
+
+
+
+
